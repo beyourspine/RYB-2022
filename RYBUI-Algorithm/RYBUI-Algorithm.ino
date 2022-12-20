5428ba51f588171858f6431d09202b8e53ee4a42
beyourspine <72161940+beyourspine@users.noreply.github.com>
2022-12-20 02:02:58 +0100

    Update RYBUI-Algorithm.ino

--- RYBUI-Algorithm/RYBUI-Algorithm.ino
+++ RYBUI-Algorithm/RYBUI-Algorithm.ino
@@ -1,68 +1,108 @@
 #include <M5Stack.h>
+#include <time.h>
+#include <Ticker.h>
+#include <math.h>
 
-#Define pulsePin 13;
-#Define freqPin 23;
-#Define ampPin 19;
-#Define volPin NULL;
+#define pulsePin 35
+#define volPin 36
+#define freqPin 25
+#define ampPin 26
+
+Ticker heartRateSample;
 
 //using structs may be execssive but it would allow me to track time and get a 
 //proper slope for extrapolating and predicting current stress bpm.
 struct heartRateStruct {
-  int BPM
-  int time
+  int BPM;
+  int time;
 };
 
-//int heartRate[50] = NULL;
-int babyVolume[50] = NULL;
-int stress[50] = NULL;
-heartRate[50] = NULL;
+//int heartRate[50] = {NULL};
+int samplesHR[10] = {NULL};
+int babyVolume[50] = {NULL};
+int stress[50] = {NULL};
+int predictedHR[50] = {NULL};
+int errorValue[10] = {NULL};
+
+struct heartRateStruct heartRate[50] = {NULL};
+
+
 
 void setup() 
 {
   M5.begin();
   M5.Power.begin();
-  Serial.begin(9600);
 
+  heartRateSample.attach_ms(1000, readHeartRate);
+
+  //pinMode(pulsePin, INPUT);
+  //pinMode(volPin, INPUT);
+  //pinMode(freqPin, OUTPUT);
+  //pinMode(ampPin, OUTPUT);
+  
   M5.Lcd.print("Setup successful");
 }
 
-int readHeartRate() //Reads heart rate then stores it in an array, may change to struct to have time function
+
+
+void readHeartRate() //Reads heart rate then stores it in an array, may change to struct to have time function
 {
-  int pulseHigh = pulseIn(pulsePin, HIGH);
-  int pulseLow = pulseIn(pulsePin, LOW);
-  int pulseTotal= pulseHigh + pulseLow;
-  int freq = 1000000/pulseTotal; //should be between 0-240bpm, have to double check if its in bpm
-  for (i = 50; i >= 0; i++)
+
+  int averageBPM = 0, averageClock = 0;
+
+  for (int i = 50; i > 0; i--)
   {
-    if (heartRate[i] != NULL)
+    if (heartRate[i - 1].BPM != NULL)
     {
       heartRate[i] = heartRate[i - 1];
     }
   }
-  heartRate[0] = freq; //returns in bpm I think
-  return heartRate[0];
+
+  for (int i = 0; i < 10000; i++)
+  {
+    //int pulseHigh = pulseIn(pulsePin, HIGH);
+    //int pulseLow = pulseIn(pulsePin, LOW);
+    //int pulseTotal= pulseHigh + pulseLow;
+    int freq = 160; //1000000/pulseTotal; //should be between 0-240bpm, have to double check if its in bpm
+
+    averageBPM = averageBPM + freq;
+    averageClock = averageClock + clock();
+
+  }
+
+  heartRate[0].BPM = averageBPM/10000;
+  heartRate[0].time = averageClock/10000;
+  M5.Lcd.println(heartRate[0].time);
+  M5.Lcd.println(heartRate[0].BPM);
+
+  if (heartRate[1].time != NULL)
+  {
+    M5.Lcd.println(heartRate[0].time - heartRate[1].time);
+  }
+
 }
+
+
 
 int readVolume() //Reads volume into an array, may change to struct to have time variable
 {
-  //Section above should include reading volume from pin and converts to between 0-100 and stores in volume then array
-  for (i = 50; i >= 0; i++)
+  int volume = 0; // Will be changed from 0 to input pin reading
+                  //Section above should include reading volume from pin and converts to between 0-100 and stores in volume then array
+  for (int i = 50; i > 0; i--)
   {
-    if (babyVolume[i] != NULL)
+    if (babyVolume[i - 1] != NULL)
     {
       babyVolume[i] = babyVolume[i - 1];
     }
   }
+
   babyVolume[0] = volume;
   return babyVolume[0];
 }
 
-void serialPrintCurrentHR() //serial port command, probably useless but might come in handy
-{
-  Serial.println(heartRate[0]);
-}
 
-void updateMotor(int Frequency, int Amplitude) //All inputs should be between 0-255
+
+void motorControl(int Frequency, int Amplitude) //All inputs should be between 0-255
 {
   if (Frequency > 255 || Amplitude > 255 || Frequency < 0 || Amplitude < 0)
   {
@@ -73,30 +113,58 @@ void updateMotor(int Frequency, int Amplitude) //All inputs should be between 0-
   analogWrite(ampPin, 255 - Amplitude);
 }
 
-void currentHeartRate() //returns in bpm
+
+
+void errorCorrection()
 {
-  readHeartRate()
+
 }
 
-int stressAlgorithm(int stress)
+
+
+int currentHeartRate() //Linear regression algorithm
 {
-  if(currentHeartRate() < 120)
+  
+
+
+  
+
+
+
+
+  return predictedHeartRate[0];
+}
+
+
+
+int stressLevel()
+{
+  for (int i = 50; i > 0; i--)
   {
-    stress = readVolume(volPin) / 2;
+    if (stress[i - 1] != NULL)
+    {
+      stress[i] = stress[i - 1];
+    }
   }
-  else if
+  if (currentHeartRate() < 120)
   {
-    stress = currentHeartRate() / 2.4;
+    stress[0] = readVolume() / 2;
+  }
+  else 
+  {
+    stress[0] = currentHeartRate() / 2.4;
   }
   //Need to figure out how amplitude and frequency of motors effects stress now
   //It may just search randomly I guess but this should be more informed atleast
 
 }
 
+
+
 void loop() 
 {
-  if ()
-
-  serialPrintCurrentHR();
+  
+  //stressLevel();
+  
 
 }
